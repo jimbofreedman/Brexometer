1d45575fcbaef4c60200623067bc2e2e326ed97e
Adam Spiers <tech4uk@adamspiers.org>
2018-09-02 23:25:03 +0100

    remove collections from front page

--- src/components/CollectionsList/CollectionsList.js
+++ src/components/CollectionsList/CollectionsList.js
@@ -1,9 +1,5 @@
 import React from 'react';
-import {Card, CardActions, CardMedia, CardTitle, CardText} from 'material-ui/Card';
-import FlatButton from 'material-ui/FlatButton';
 import { observer, inject } from "mobx-react";
-import { Link } from 'react-router-dom';
-import CollectionSearch from '../CollectionSearch';
 import {Helmet} from "react-helmet";
 import RaisedButton from 'material-ui/RaisedButton';
 //import smallLogo from './represent_white_outline.svg';
@@ -17,8 +13,6 @@ const CollectionsList = inject("CollectionStore", "UserStore")(observer(({ Colle
   if (CollectionStore.collections.size <= 0) {
     return null;
   }
-
-  let collections = CollectionStore.collections.entries();
 
   return (
     <div>
@@ -96,67 +90,6 @@ const CollectionsList = inject("CollectionStore", "UserStore")(observer(({ Colle
           </div>
         </div>
 
-      </div>
-
-      <div><CollectionSearch /></div>
-      <div className='containerStyles'>
-      {collections.map((collection_obj) => {
-        const id = collection_obj[0];
-        const collection = collection_obj[1];
-
-        // const first_name = collection.user.first_name ? collection.user.first_name : '';
-        // const last_name = collection.user.last_name ? collection.user.last_name : '';
-        // const user_name = `${first_name} ${last_name}`;
-        // const bio = collection.user.bio ? collection.user.bio : '';
-        // const location = collection.user.country_info ? collection.user.country_info.name : '';
-        // const randomPic = `./img/pic${Math.floor(Math.random()*7)}.png`;
-        //const photo = collection.user.photo ? collection.user.photo.replace("localhost:8000", "represent.me") : randomPic;
-        const image = collection.photo ? collection.photo.replace("localhost:8000", "represent.me") : null;
-        //const link = "https://app.represent.me/profile/" + collection.user.id + "/" + collection.user.username; //our user
-        //const subtitle = `${bio.substring(0, location ? 77-location.length : 77)}${bio && '...'} ${location}`
-
-        return (
-
-            <Card className='cardStyles' key={ id }>
-
-            <Link to={ "/survey/" + id } >
-               {/* <CardHeader
-                    title={user_name}
-                    subtitle={subtitle}
-                    avatar={photo}
-                    className='cardHeaderStyle'
-                />
-              */}
-
-                <CardMedia>
-                  <img src={image} className='imgStyle' alt='' />
-                </CardMedia>
-                <CardTitle
-                  className='cardTitle'
-                  title={ collection.name }
-                />
-              </Link>
-              <CardText style={{wordWrap: 'break-word'}} className='cardText'>
-
-              <div style={{margin:'0 0 3px 0', fontSize: 11, color: '#999'}}>{collection.question_count} questions</div>
-              {collection.desc ?
-                <div>
-                  {collection.desc.slice(0, 100 + collection.desc.indexOf(' ')) + ' '}
-                  <Link to={ "/survey/" + id }><i>more...</i></Link>
-                </div>
-                : null}
-              </CardText>
-
-              <CardActions>
-                <Link to={ "/survey/" + id }>
-                  <FlatButton label="Start" primary />
-                 </Link>
-              </CardActions>
-
-            </Card>
-
-        )
-      })}
       </div>
       <OgTags />
     </div>
