115f040988fe363ccf3ffedb9fb0a0732d104eb7
Jimbo Freedman <jimbo@project-sapiens.com>
2018-11-18 12:35:12 +0000

    Cleanup

--- src/components/Logout/Logout.jsx
+++ src/components/Logout/Logout.jsx
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
 import { observer, inject } from "mobx-react";
-import { Redirect } from 'react-router'
+import { Redirect } from 'react-router-dom'
 
 @inject("UserStore", "routing")
 @observer
 export default class Logout extends Component {
   componentDidMount() {
-    const { UserStore, routing } = this.props;
+    const { UserStore } = this.props;
     UserStore.logout();
   }
 
   render() {
     // Todo show a message telling the user they are logged out
-    return <Redirect to="/" />
+    return <Redirect to="/" />;
   }
 }
